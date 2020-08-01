ee96cfc04afe3738a417061f46960be9bb0b4d7a
Joanna Lamch <jlamch@gmail.com>
2020-08-01 21:19:37 +0000

    quize overview, quize detail

---
+++ src/Johoot.Workshop.UI/Pages/QuizeDetailBase.cs
@@ -0,0 +1,23 @@
+﻿using Johoot.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Johoot.Workshop.UI.Pages
+{
+  public class QuizeDetailBase : ComponentBase
+  {
+    [Parameter]
+    public int QuizeId { get; set; }
+    public Quize Quize { get; set; } = new Quize();
+
+    protected override void OnInitialized()
+    {
+      base.OnInitialized();
+      Quize = new Quize
+      {
+        Description = "Simulated quize description",
+        Id = 1,
+        Name = "First Quize"
+      };
+    }
+  }
+}
---
+++ src/Johoot.Workshop.UI/Pages/QuizeOverviewBase.cs
@@ -0,0 +1,21 @@
+﻿using Johoot.Data;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Johoot.Workshop.UI.Pages
+{
+  public class QuizeOverviewBase : ComponentBase
+  {
+    public IList<Quize> Quizes { get; set; }
+    protected override void OnInitialized()
+    {
+      base.OnInitialized();
+      Quizes = new List<Quize> {
+        new Quize {
+          Description= "Simulated quize description",
+          Id =1,
+          Name = "First Quize"}
+      };
+    }
+  }
+}
